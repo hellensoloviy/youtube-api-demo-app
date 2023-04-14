bce3e1fc13af90212a5ffe6a362351025735e64d
Hellen S <hellensoloviy@gmail.com>
2023-04-14 10:25:25 +0300

    fixed UI and layouts for search screen

--- Youtube-demo-app/ViewControllers/Search/Cell/SearchResultVideoTVC.swift
+++ Youtube-demo-app/ViewControllers/Search/Cell/SearchResultVideoTVC.swift
@@ -6,26 +6,36 @@
 //
 
 import UIKit
+import Combine
 
 class SearchResultVideoTVC: UITableViewCell {
     
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var thumbnail: UIImageView!
-    
-//    @IBOutlet weak var dateAdded: UILabel!
-//    @IBOutlet weak var likes: UILabel!
+    private var model: VideoSnippetModel? = nil
+    private var cancellable: AnyCancellable? = nil
     
     // MARK: -
     override func prepareForReuse() {
         super.prepareForReuse()
         nameLabel.text = ""
-//        dateAdded.text = ""
-//        likes.text = ""
+
         thumbnail.image = nil
+        model = nil
+        cancellable?.cancel()
+        cancellable = nil
     }
     
     func bindModel(_ model: VideoSnippetModel) {
         self.nameLabel.text = model.snippet?.title ?? "oops! T_T"
+        
+        guard let urlString = model.snippet?.thumbnails?.medium?.url, let url = URL(string: urlString) else { return }
+        cancellable = URLSession.shared
+            .downloadTaskPublisher(for: url)
+            .map { UIImage(contentsOfFile: $0.url.path)! }
+            .receive(on: DispatchQueue.main)
+            .replaceError(with: UIImage(named: "")) // TODO: - add default pic
+            .assign(to: \.image, on: self.thumbnail)
         
     }
     
